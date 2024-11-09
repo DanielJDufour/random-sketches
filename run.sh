78acf13dc545230f86bc2c22cdec12b0b4116f7c
DanielJDufour <daniel.j.dufour@gmail.com>
2024-11-09 18:13:22 -0500

    added run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 ct=0;
 
-for i in $(seq 1 100);
+for i in $(seq 1 10000);
 do
   for numPolygons in $(seq 1 10);
   do
     ct=$((ct+1));
     ./node_modules/@seasketch/geoprocessing/dist/base-project/scripts/genRandomFeature.ts $numPolygons "sketch$ct" "$PWD/";
+    zip -u sketches.zip ./sketch*.json;
+    rm ./sketch*.json;
   done
 done
 
-zip sketches.zip ./sketch*.json;
